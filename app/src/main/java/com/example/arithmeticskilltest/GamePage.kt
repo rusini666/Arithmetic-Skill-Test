a359b9df1dc7b512b152de4cf85e3f37a0f6e3e3
Rusini Thara <72293107+rusini666@users.noreply.github.com>
2022-02-23 21:45:44 +0530

    Added game activity

---
+++ app/src/main/java/com/example/arithmeticskilltest/GamePage.kt
@@ -0,0 +1,11 @@
+package com.example.arithmeticskilltest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GamePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_page)
+    }
+}
--- app/src/main/java/com/example/arithmeticskilltest/MainActivity.kt
+++ app/src/main/java/com/example/arithmeticskilltest/MainActivity.kt
@@ -1,51 +1,92 @@
 package com.example.arithmeticskilltest
 
+import android.app.ActionBar
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.MotionEvent
-import android.view.View
-import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.PopupWindow
+import android.transition.Slide
+import android.transition.TransitionManager
+import android.view.*
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val newGameBtn = findViewById<Button>(R.id.newGameBtn);
-        val aboutBtn = findViewById<Button>(R.id.aboutBtn);
+        val newGameBtn = findViewById<Button>(R.id.newGameBtn)
+        val aboutBtn = findViewById<Button>(R.id.aboutBtn)
+
 
         newGameBtn.setOnClickListener {
-            val gamePage = Intent(this, GamePage::class.java);
+            val gamePage = Intent(this, GamePage::class.java)
             startActivity(gamePage)
         }
 
         aboutBtn.setOnClickListener {
-            // inflate the layout of the popup window
-            val inflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            val popupView: View = inflater.inflate(android.R.layout.about_window, null)
+            // Initialize a new layout inflater instance
+//            val inflater:LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+//
+//            // Inflate a custom view using layout inflater
+//            val view = inflater.inflate(R.layout.about_window,null)
+//
+//            // Initialize a new instance of popup window
+//            val popupWindow = PopupWindow(
+//                view, // Custom view to show in popup window
+//                LinearLayout.LayoutParams.WRAP_CONTENT, // Width of popup window
+//                LinearLayout.LayoutParams.WRAP_CONTENT // Window height
+//            )
+//
+//
+//            // If API level 23 or higher then execute the code
+//            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+//                // Create a new slide animation for popup window enter transition
+//                val slideIn = Slide()
+//                slideIn.slideEdge = Gravity.TOP
+//                popupWindow.enterTransition = slideIn
+//
+//                // Slide animation for popup window exit transition
+//                val slideOut = Slide()
+//                slideOut.slideEdge = Gravity.RIGHT
+//                popupWindow.exitTransition = slideOut
+//
+//            }
 
-            // create the popup window
-            val width = LinearLayout.LayoutParams.WRAP_CONTENT
-            val height = LinearLayout.LayoutParams.WRAP_CONTENT
-            val focusable = true // lets taps outside the popup also dismiss it
-            val popupWindow = PopupWindow(popupView, width, height, focusable)
+//            // Get the widgets reference from custom view
+//            val tv = view.findViewById<TextView>(R.id.text_view)
+//            val buttonPopup = view.findViewById<Button>(R.id.button_popup)
+//
+//            // Set click listener for popup window's text view
+//            tv.setOnClickListener{
+//                // Change the text color of popup window's text view
+//                tv.setTextColor(Color.RED)
+//            }
+//
+//            // Set a click listener for popup's button widget
+//            buttonPopup.setOnClickListener{
+//                // Dismiss the popup window
+//                popupWindow.dismiss()
+//            }
 
-            // show the popup window
-            // which view you pass in doesn't matter, it is only used for the window token
-            popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
-
-            // dismiss the popup window when touched
-            popupView.setOnTouchListener(object : View.OnTouchListener() {
-                override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-                    popupWindow.dismiss()
-                    return true
-                }
-            })
+//            // Set a dismiss listener for popup window
+//            popupWindow.setOnDismissListener {
+//                Toast.makeText(applicationContext,"Popup closed", Toast.LENGTH_SHORT).show()
+//            }
+//
+//
+//            // Finally, show the popup window on app
+//            TransitionManager.beginDelayedTransition(view as ViewGroup?)
+//            popupWindow.showAtLocation(
+//                view, // Location to display popup window
+//                Gravity.CENTER, // Exact position of layout to display popup
+//                0, // X offset
+//                0 // Y offset
+//            )
         }
+
     }
 }
