a87fd19f2ea5122e62e92422fca909c754633c1a
Rusini Thara <72293107+rusini666@users.noreply.github.com>
2022-02-26 23:34:33 +0530

    Added main functionality

--- app/src/main/java/com/example/arithmeticskilltest/GameOver.kt
+++ app/src/main/java/com/example/arithmeticskilltest/GameOver.kt
@@ -1,11 +1,18 @@
 package com.example.arithmeticskilltest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class GameOver : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_over)
+
+    }
+
+    override fun onBackPressed() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
--- app/src/main/java/com/example/arithmeticskilltest/GamePage.kt
+++ app/src/main/java/com/example/arithmeticskilltest/GamePage.kt
@@ -1,11 +1,17 @@
 package com.example.arithmeticskilltest
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import kotlin.random.Random
 
 class GamePage : AppCompatActivity() {
+    val correctCount = 0
+    val wrongCount = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_page)
@@ -13,64 +19,118 @@ class GamePage : AppCompatActivity() {
         val greaterBtn =  findViewById<Button>(R.id.greaterBtn)
         val equalsBtn =  findViewById<Button>(R.id.equalsBtn)
         val lessBtn = findViewById<Button>(R.id.lessBtn)
-        val leftExp= findViewById<TextView>(R.id.leftExp)
+        val leftExp = findViewById<TextView>(R.id.leftExp)
         val rightExp = findViewById<TextView>(R.id.rightExp)
-
-        var num1 = (0..20).random()
-        var num2 = (0..20).random()
-        var num3 = (0..20).random()
-        var num4 = (0..20).random()
+        val resultDisplay = findViewById<TextView>(R.id.resultDisplay)
+        val timer = findViewById<TextView>(R.id.timer)
 
 
-        var numberOfExpressions = 10
-        var minExpressionElements = 2
-        var maxExpressionElements = 4
-        var minRange = 1
-        var maxRange = 20
-        val list = ArrayList<Int?>()
-        val operators = arrayOf("+","-","/","*")
-        var correctAns = 0
-        val index: Int = (0..3).random()
-        when (index) {
-            0 -> correctAns = num1 + num2
-            1 -> correctAns = num1 - num2
-            2 -> correctAns = num1 / num2
-            3 -> correctAns = num1 * num2
+
+        val finish = Intent(this, GameOver::class.java)
+        object : CountDownTimer(50000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                timer.text = "Seconds remaining: " + millisUntilFinished / 1000
+            }
+
+            override fun onFinish() {
+                finish.putExtra("correct", correctCount.toString())
+                finish.putExtra("wrong", wrongCount.toString())
+                startActivity(finish)
+            }
+        }.start()
+
+
+        fun mainFunc(){
+//            resultDisplay.text = ""
+
+
+            val left = randomExpression()
+            val right = randomExpression()
+            leftExp.text = left
+            rightExp.text = right
+
+            greaterBtn.setOnClickListener {
+                if(answer(left) > answer(right)) {
+                    resultDisplay.text = "CORRECT!"
+                    resultDisplay.setTextColor(ContextCompat.getColor(this, R.color.green))
+                    mainFunc()
+                }else {
+                    resultDisplay.text = "WRONG!"
+                    resultDisplay.setTextColor(ContextCompat.getColor(this, R.color.red))
+                    mainFunc()
+                }
+            }
+            equalsBtn.setOnClickListener {
+                if(answer(left) == answer(right)) {
+                    resultDisplay.text = "CORRECT!"
+                    resultDisplay.setTextColor(ContextCompat.getColor(this, R.color.green))
+                    mainFunc()
+                }else {
+                    resultDisplay.text = "WRONG!"
+                    resultDisplay.setTextColor(ContextCompat.getColor(this, R.color.red))
+                    mainFunc()
+                }
+            }
+            lessBtn.setOnClickListener {
+                if(answer(left) < answer(right)) {
+                    resultDisplay.text = "CORRECT!"
+                    resultDisplay.setTextColor(ContextCompat.getColor(this, R.color.green))
+                    mainFunc()
+                }else {
+                    resultDisplay.text = "WRONG!"
+                    resultDisplay.setTextColor(ContextCompat.getColor(this, R.color.red))
+                    mainFunc()
+                }
+            }
         }
+        mainFunc()
 
-        when (index) {
-            0 -> correctAns = num1 + num2
-            1 -> correctAns = num1 - num2
-            2 -> correctAns = num1 / num2
-            3 -> correctAns = num1 * num2
-        }
-
-        leftExp.text = "$num1" + " " + operators.get(index) + " " + "$num2"
-
-
-
-
-
-//        fun twoExpressions(){
-//            for (i in 0..numberOfExpressions)
-//                list.add("$num1 $operator.get((0..3).random()) $num2")
-//        }
-//        fun threeExpressions(left: Int, right: Int)
-//        fun fourExpressions(left: Int, right: Int)
-
-        greaterBtn.setOnClickListener {
-
-        }
-        equalsBtn.setOnClickListener {
-
-        }
-        lessBtn.setOnClickListener {
-
-        }
     }
-}
 
-private fun <E> ArrayList<E>.add(element: String) {
+    private val operators = listOf("+", "-", "/", "*")
+
+    private fun randomNumber(): Int = Random.nextInt(1, 20)
+
+    private fun randomExpression(): String {
+        val numberOfExps = Random.nextInt(1, 4)
+        val arithmeticExp = mutableListOf("${randomNumber()}")
+
+        repeat(numberOfExps) {
+            arithmeticExp.add(operators.random())
+            arithmeticExp.add("${randomNumber()}")
+        }
+
+        return arithmeticExp.joinToString(separator = "")
+    }
+
+    private fun answer(arithmeticExp: String): Int {
+        var num: String = ""
+        var symbol: Char = '+'
+        var finalAns: Int = 0
+
+        for(i in arithmeticExp) {
+            if (i in '0'..'9')
+                num += i
+            else {
+                when(symbol){
+                    '+' -> finalAns += Integer.parseInt(num)
+                    '-' -> finalAns -= Integer.parseInt(num)
+                    '*' -> finalAns *= Integer.parseInt(num)
+                    '/' -> finalAns /= Integer.parseInt(num)
+                }
+                num = ""
+                symbol = i
+            }
+        }
+        when(symbol){
+            '+' -> finalAns += Integer.parseInt(num)
+            '-' -> finalAns -= Integer.parseInt(num)
+            '*' -> finalAns *= Integer.parseInt(num)
+            '/' -> finalAns /= Integer.parseInt(num)
+        }
+        return finalAns
+    }
 
 }
 
