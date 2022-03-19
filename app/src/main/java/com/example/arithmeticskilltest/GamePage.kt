d87c9a31633bbd66cc5386018ea27dc60572be64
Rusini Thara <72293107+rusini666@users.noreply.github.com>
2022-03-19 21:02:12 +0530

    Saved data on orientation change

--- app/src/main/java/com/example/arithmeticskilltest/GamePage.kt
+++ app/src/main/java/com/example/arithmeticskilltest/GamePage.kt
@@ -3,49 +3,37 @@ package com.example.arithmeticskilltest
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.view.View
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
-
 
 class GamePage : AppCompatActivity() {
     var correctCount = 0
     var wrongCount = 0
     var currentTime : Long = 0
-    lateinit var count_down_timer : CountDownTimer
+    private lateinit var count_down_timer : CountDownTimer
+    var formattedLeft = ""
+    var formattedRight = ""
+    private lateinit var leftExp : TextView
+    private lateinit var rightExp : TextView
+    lateinit var timer : TextView
+    lateinit var finish : Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_page)
 
-        val timer: TextView = findViewById(R.id.timer)
+        timer = findViewById(R.id.timer)
         val greaterBtn = findViewById<Button>(R.id.greaterBtn)
         val equalsBtn = findViewById<Button>(R.id.equalsBtn)
         val lessBtn = findViewById<Button>(R.id.lessBtn)
-        val leftExp = findViewById<TextView>(R.id.leftExp)
-        val rightExp = findViewById<TextView>(R.id.rightExp)
+        leftExp = findViewById(R.id.leftExp)
+        rightExp = findViewById(R.id.rightExp)
         val resultDisplay = findViewById<TextView>(R.id.resultDisplay)
-        val finish = Intent(this, GameOver::class.java)
+        finish = Intent(this, GameOver::class.java)
 
-
-
-        fun timerFunc(milliseconds: Long){
-            count_down_timer = object: CountDownTimer(milliseconds, 1000){
-                override fun onTick(millisUntilFinished: Long) {
-                    currentTime = millisUntilFinished
-                    timer.text = "" + currentTime / 1000
-                }
-
-                override fun onFinish() {
-                    finish.putExtra("correct", correctCount.toString())
-                    finish.putExtra("wrong", wrongCount.toString())
-                    startActivity(finish)
-                }
-            }.start()
-        }
 
         /**
          *
@@ -53,9 +41,9 @@ class GamePage : AppCompatActivity() {
          *
          */
         fun bonusTime() {
-            if(correctCount % 5 == 0 && correctCount != 0) {
+            if (correctCount % 5 == 0 && correctCount != 0) {
                 count_down_timer.cancel()
-                timerFunc(currentTime+10000)
+                timerFunc(currentTime + 10000)
             }
         }
 
@@ -113,10 +101,44 @@ class GamePage : AppCompatActivity() {
                     mainFunc()
                 }
             }
-            leftExp.text = f1.getFormattedExpression(left)
-            rightExp.text = f1.getFormattedExpression(right)
+            formattedLeft = f1.getFormattedExpression(left)
+            formattedRight = f1.getFormattedExpression(right)
+            leftExp.text = formattedLeft
+            rightExp.text = formattedRight
         }
         mainFunc()
+    }
+
+    private fun timerFunc(milliseconds: Long){
+        count_down_timer = object: CountDownTimer(milliseconds, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                currentTime = millisUntilFinished
+                timer.text = "" + currentTime / 1000
+            }
+
+            override fun onFinish() {
+                finish.putExtra("correct", correctCount.toString())
+                finish.putExtra("wrong", wrongCount.toString())
+                startActivity(finish)
+            }
+        }.start()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("LEFT_RESULT", formattedLeft)
+        outState.putString("RIGHT_RESULT", formattedRight)
+        outState.putLong("TIMER_RESULT",currentTime)
+     }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        formattedLeft = savedInstanceState.getString("LEFT_RESULT", "")
+        leftExp.text = formattedLeft
+        formattedRight = savedInstanceState.getString("RIGHT_RESULT", "")
+        rightExp.text = formattedRight
+        currentTime = savedInstanceState.getLong("TIMER_RESULT", 0)
+        timerFunc(currentTime)
     }
 
 }
