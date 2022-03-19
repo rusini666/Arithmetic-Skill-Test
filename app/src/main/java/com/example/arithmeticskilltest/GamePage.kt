4215e1dce752bba73958036632da0a9d1cf9f576
Rusini Thara <72293107+rusini666@users.noreply.github.com>
2022-03-19 14:54:20 +0530

    Modified the application

--- app/src/main/java/com/example/arithmeticskilltest/GamePage.kt
+++ app/src/main/java/com/example/arithmeticskilltest/GamePage.kt
@@ -3,10 +3,13 @@ package com.example.arithmeticskilltest
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
+
 
 class GamePage : AppCompatActivity() {
     var correctCount = 0
@@ -17,6 +20,7 @@ class GamePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_page)
+
         val timer: TextView = findViewById(R.id.timer)
         val greaterBtn = findViewById<Button>(R.id.greaterBtn)
         val equalsBtn = findViewById<Button>(R.id.equalsBtn)
@@ -25,6 +29,7 @@ class GamePage : AppCompatActivity() {
         val rightExp = findViewById<TextView>(R.id.rightExp)
         val resultDisplay = findViewById<TextView>(R.id.resultDisplay)
         val finish = Intent(this, GameOver::class.java)
+
 
 
         fun timerFunc(milliseconds: Long){
